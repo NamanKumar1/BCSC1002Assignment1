d80951084bc1c7ff4e60ea0ec0acdaaf7a72ae34
Naman <naman.kumar_cs19@gla.ac.in>
2020-08-30 17:53:45 +0530

    apply switch case for user

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,8 +6,60 @@
  * */
 package execution;
 
-public class FrontDesk {
-    public static void main(String[] args) {
+import definitions.Book;
+import definitions.Library;
+import definitions.Student;
 
+import java.util.Scanner;
+
+public class FrontDesk {
+    private static final int ISSUE_A_NEW_BOOK_FOR_ME = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int MY_ISSUES_BOOKS = 3;
+    private static final int EXIT = 4;
+
+    public static void main(String[] args) {
+        Student currentlyInteractStudent;
+        Scanner scannerObject = new Scanner(System.in);
+        int studentInput;
+
+        Library libraryObject = new Library();
+        Student studentObject = new Student();
+        Book bookObject = new Book();
+
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?\n");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter you choice (1..4): ");
+            studentInput = scannerObject.nextInt();
+            switch (studentInput) {
+                case ISSUE_A_NEW_BOOK_FOR_ME:
+                    scannerObject.nextLine();
+                    System.out.println("Enter the name of the books that you want to issue: ");
+                    String[] bookNames = new String[5];
+                    for (int i = 0; i < 5; i++) {
+                        bookNames[i] = scannerObject.next();
+                    }
+                    for (String bookName : bookNames) {
+                        System.out.println(bookName);
+                    }
+                case RETURN_BOOK:
+                    System.out.println("Enter the name of the book you want to return: ");
+                    scannerObject.nextLine();
+                    break;
+                case MY_ISSUES_BOOKS:
+                    System.out.println("Your all issues book names are: ");
+                    scannerObject.nextLine();
+
+                    break;
+
+                default:
+            }
+        } while (studentInput != EXIT);
+        scannerObject.close();
     }
 }
